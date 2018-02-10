8acd9c20b1657be45009afc3ecc1c59f391405e2
deimonfree <deimonfree22@gmail.com>
2018-02-10 16:36:10 -0500

    ventana principal tesoreria
    inicio de ventana principal del modulo de tesoreria

---
+++ Modulo Tesoreria Munisoft/Modulo Tesoreria Munisoft/login.xaml.vb
@@ -0,0 +1,14 @@
+﻿Public Class login
+    Private Sub Border_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs)
+        DragMove()
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Me.Close()
+    End Sub
+    Private Sub Sesion_Click(sender As Object, e As RoutedEventArgs)
+        Dim dashboard = New MainWindow
+        dashboard.Show()
+        Me.Close()
+    End Sub
+End Class
