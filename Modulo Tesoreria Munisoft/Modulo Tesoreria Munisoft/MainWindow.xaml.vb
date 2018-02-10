dc21e5659039bfddfa73c1209276a24151ded05a
deimonfree <deimonfree22@gmail.com>
2018-02-10 15:32:42 -0500

    interfaz de login
    interfaz de login en wpf

---
+++ Modulo Tesoreria Munisoft/Modulo Tesoreria Munisoft/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub Border_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Me.Close()
+    End Sub
+End Class
